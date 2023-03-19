f8e2c6f51245fac5ad047b903b4c582399c1ffe7
Beka Sturua <beqasturua3@gmail.com>
2023-03-19 18:19:43 +0400

    feat: add forgotPassword and resetPassword

--- routers/auth.router.ts
+++ routers/auth.router.ts
@@ -1,6 +1,6 @@
 
 import express, { Request, Response } from 'express';
-import { login, register } from '../services/auth.service.';
+import { forgotPassword, login, register, resetPassword } from '../services/auth.service.';
 import { body, validationResult } from 'express-validator';
 
 
@@ -45,6 +45,48 @@ authRouter.post("/login", [
         res.status(200).send({ token })
     } catch (error: any) {
         res.status(404).send(error.message)
+    }
+});
+
+
+authRouter.post("/forgot-password", [
+    body('email').isEmail(),
+], async (req: Request, res: Response) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email } = req.body;
+
+        const forgotPasswordToken = await forgotPassword(email)
+
+        res.status(200).send({ forgotPasswordToken })
+    } catch (error: any) {
+        res.status(404).send(error.message)
+    }
+});
+
+authRouter.post("/reset-password", [
+    body('forgotPasswordToken').isString().notEmpty(),
+    body('password').isLength({ min: 6 }),
+    body('repeatPassword').isLength({ min: 6 }),
+
+], async (req: Request, res: Response) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { forgotPasswordToken, password, repeatPassword } = req.body;
+
+        await resetPassword(forgotPasswordToken, password, repeatPassword)
+
+        res.status(200).send("Password reseted succesfully")
+    } catch (error: any) {
+        res.status(400).send(error.message)
     }
 });
 
--- services/auth.service..ts
+++ services/auth.service..ts
@@ -36,4 +36,54 @@ export const login = async (email: string, password: string) => {
         throw error;
 
     }
-} 
+}
+
+export const forgotPassword = async (email: string) => {
+
+    const db = getDb();
+    try {
+        const user: User | null = await db.collection("users").findOne<User>({ email });
+
+        if (user === null) {
+            throw new Error("User with this email not found!");
+        }
+
+        const forgotPasswordToken = jwt.sign({ email, purpose: "reset_password" }, process.env.JWT_TOKEN as string);
+
+        return forgotPasswordToken;
+    } catch (error) {
+        throw error;
+
+    }
+}
+
+
+export const resetPassword = async (forgotPasswordToken: string, password: string, repeatPassword: string) => {
+
+    const db = getDb();
+    try {
+        jwt.verify(forgotPasswordToken, process.env.JWT_TOKEN as string);
+
+        const decoded: any = jwt.decode(forgotPasswordToken);
+
+        const email = decoded["email"];
+        const purpose = decoded['purpose'];
+
+        if (purpose !== 'reset_password') {
+            throw new Error("Token purpose is not reset password");
+        }
+        if (password !== repeatPassword) {
+            throw new Error("Password and repeatPassword are not same.");
+        }
+
+        const hashedPassword = await hash(password, 10);
+
+        await db.collection("users").updateOne({ email }, { $set: { hashedPassword } });
+
+
+
+    } catch (error) {
+        throw error;
+
+    }
+}
