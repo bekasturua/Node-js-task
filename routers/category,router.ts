acfa63a3cc2ea81735e11887a516c1312b3be87b
Beka Sturua <beqasturua3@gmail.com>
2023-03-24 01:16:09 +0400

    feat: add delete category

--- routers/category,router.ts
+++ routers/category,router.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, param, validationResult } from 'express-validator';
 import { verifyToken } from '../middleware/token.middleware';
-import { createCategory, updateCategory } from '../services/category.service';
+import { createCategory, deleteCategory, updateCategory } from '../services/category.service';
 import { User } from '../types/user.type';
 
 
@@ -45,6 +45,27 @@ categoryRouter.put("/categories/:name", [
         res.status(200).send({ updatedCategory })
     } catch (error: any) {
         res.status(409).send(error.message)
+    }
+});
+
+
+categoryRouter.delete("/categories/:name", [param('name').isString().notEmpty(),], verifyToken, async (req: Request, res: Response) => {
+
+
+    try {
+        
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name } = req.params;
+
+        const result = await deleteCategory(name, (req as any).user as User);
+
+        res.status(200).send({ message: "Category deleted successfully" });
+    } catch (error: any) {
+        res.status(400).send(error.message);
     }
 });
 
--- services/category.service.ts
+++ services/category.service.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { getDb } from "../database";
 import { User } from "../types/user.type";
 
@@ -30,3 +31,44 @@ export const updateCategory = async (name: string, newName: string, user: User) 
         throw error;
     }
 }
+
+
+export const deleteCategory = async (name: string, user: User) => {
+
+    const db = getDb();
+
+    try {
+        const category = await db.collection("categories").findOne({ name, user_id: user._id });
+        const defaultCategory = await db.collection("categories").findOne({ name: "default" });
+
+        if (category === null) {
+            throw new Error("Category not found");
+        }
+        const transactions = await db.collection("transactions").find({
+            "category_ids": { $elemMatch: { $eq: category._id } }
+        }).toArray();
+
+        transactions.map(async (transaction) => {
+            const categoryIds = transaction.category_ids.map((category_id: ObjectId) => category_id.toString())
+            const categoryToDeleteIndex = categoryIds.indexOf(category._id.toString());
+
+            transaction.category_ids.splice(categoryToDeleteIndex, 1);
+
+            const defaultCategoryIndex = categoryIds.indexOf(defaultCategory?._id.toString());
+            if (defaultCategoryIndex === -1) {
+                transaction.category_ids.push(defaultCategory?._id);
+            }
+            await db.collection("transactions").updateOne({ _id: transaction._id }, { $set: { category_ids: transaction.category_ids } });
+        })
+
+        await db.collection("categories").deleteOne({ _id: category._id });
+
+
+
+        return category;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
